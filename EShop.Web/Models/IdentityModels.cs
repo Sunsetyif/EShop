74f757d4decd36d62d6da1193252f79eaa3e2bd4
Andrew K <drunyakk@gmail.com>
2019-06-17 02:15:00 +0300

    union 2 database, fix user UI

--- EShop.Web/Models/IdentityModels.cs
+++ EShop.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace EShop.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EShopConnection", throwIfV1Schema: false)
         {
         }
 
